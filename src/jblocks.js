7d1101a861011028c2a2ae899467820dc7095276
Sergey Savelev <sergcen@bitbucket.org>
2015-10-06 19:08:13 +0300

    move methods object to parent scope

--- src/jblocks.js
+++ src/jblocks.js
@@ -1,6 +1,53 @@
 var helpers = require('./helpers');
 var Block = require('./Block');
 
+var methods = {
+    /**
+     * Init all blocks inside
+     */
+    'init': function () {
+        return this.find('[data-b]').jblocks('get');
+    },
+    /**
+     * Destroy all blocks
+     */
+    'destroy': function () {
+        this.find('[data-b]').jblocks('get').each(function () {
+            this.destroy();
+        });
+    },
+    /**
+     * Returns block from cache or create it if doesn't exist
+     * @return {Block} block
+     */
+    'get': function () {
+        return this.map(function () {
+            var $b = $(this);
+            var bid = $b.data('_bid');
+
+            if (bid) {
+                return helpers.cache[bid];
+            }
+
+            var block = new Block($b);
+            bid = block._id;
+
+            $b.data('_bid', bid);
+            helpers.cache[bid] = block;
+
+            return block;
+        });
+    }
+};
+
+$.fn.jblocks = function (method) {
+    if (method in methods) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else {
+        throw new Error('Can`t find method ' + method);
+    }
+};
+
 /**
  * Block's declaration
  * @param  {Object} proto
@@ -15,51 +62,7 @@ $.jblocks = function (proto) {
     helpers.decls[proto.name] = proto;
 };
 
-$.fn.jblocks = function (method) {
-    var methods = {
-        /**
-         * Init all blocks inside
-         */
-        'init': function () {
-            return this.find('[data-b]').jblocks('get');
-        },
-        /**
-         * Destroy all blocks
-         */
-        'destroy': function () {
-            this.find('[data-b]').jblocks('get').each(function () {
-                this.destroy();
-            });
-        },
-        /**
-         * Returns block from cache or create it if doesn't exist
-         * @return {Block} block
-         */
-        'get': function () {
-            return this.map(function () {
-                var $b = $(this);
-                var bid = $b.data('_bid');
 
-                if (bid) {
-                    return helpers.cache[bid];
-                }
-
-                var block = new Block($b);
-                bid = block._id;
-
-                $b.data('_bid', bid);
-                helpers.cache[bid] = block;
-
-                return block;
-            });
-        }
-    };
-    if (method in methods) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    } else {
-        throw new Error('Can`t find method ' + method);
-    }
-};
 
 // to get Block from global outspace
 $.Block = Block;
