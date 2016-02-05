f94cf76befe159fd91795e2485dbc3d96bfcca7a
vitkarpov <viktor.s.karpov@gmail.com>
2016-02-05 11:37:52 +0300

    jsdoc

--- jBlocks.js
+++ jBlocks.js
@@ -5,18 +5,110 @@ var jBlocks = (function() {
     var noop = function() {};
 
     /**
-     * Creates a new instance of component
-     * @constructor
+     * @namespace
+     * @name jBlocks
+     * @description
+     * jBlocks containts static methods to define components
+     * using declaration and create new instances.
+     * Also helps to find and destroy components.
+     */
+    var jBlocks = {};
+
+    /**
+     * Destroy instance binded to the node
+     * @memberof jBlocks
+     * @param  {HTMLElement} node
+     * @return {jBlocks}
+     */
+    jBlocks.destroy = function(node) {
+        this.get(node).destroy();
+        return this;
+    };
+
+    /**
+     * Define a new component
+     * @memberof jBlocks
+     * @param  {String} name
+     * @param  {Object} declaration
+     * @return {jBlocks}
+     */
+    jBlocks.define = function(name, declaration) {
+        if (declarations[name]) {
+            throw new Error(name + ' has already been declared');
+        }
+        declarations[name] = declaration || {};
+        return this;
+    },
+
+    /**
+     * Remove declaration from cache
+     * @memberof jBlocks
+     * @param  {String} name name of component
+     * @return {jBlocks}
+     */
+    jBlocks.forget = function(name) {
+        declarations[name] = null;
+        return this;
+    },
+
+    /**
+     * Create and return a new instance of component
+     * @memberof jBlocks
+     * @param  {HTMLElement}  node
+     * @return {jBlocks.Component} a new instance
+     */
+    jBlocks.get = function(node) {
+        if (!node) {
+            throw new Error('invalid node');
+        }
+        var name = node.getAttribute('data-component');
+        if (!name) {
+            throw new Error('data-component attribute is missing')
+        }
+        var instanceId = node.getAttribute('data-instance');
+        if (!instanceId) {
+            try {
+                var props = JSON.parse(node.getAttribute('data-props'));
+            } catch (e) {
+                throw e;
+            }
+            var instance = new Component(node, name, props);
+            var instanceId = instance.__id;
+
+            if (!instances[name]) {
+                instances[name] = {};
+            }
+            instances[name][instanceId] = instance;
+        }
+        return instances[name][instanceId];
+    };
+
+    /**
+     * Create a new instance of component
+     * @class
+     * @memberof jBlocks
      * @param {HTMLElement} node
      * @param {String}      name
      * @param {Object}      props
      */
     var Component = function(node, name, props) {
+        /**
+         * Name of the components used in decl
+         * @type {String}
+         */
         this.name = name;
+        /**
+         * Node which component is binded with
+         * @type {HTMLElement}
+         */
         this.node = node;
+        /**
+         * Props of the component gained from data-props
+         * @type {Object}
+         */
         this.props = props || {};
-        this.decl = declarations[this.name] || {};
 
+        this.__decl = declarations[this.name] || {};
         this.__id = ++gid;
         this.__events = {};
         this.__handlerDomEvents = this.__handlerDomEvents.bind(this);
@@ -25,196 +117,201 @@ var jBlocks = (function() {
 
         this.oninit();
     };
+    jBlocks.Component = Component;
 
-    Component.prototype.on = function(event, callback) {
-        this.__events[event] = this.__events[event] || [];
+    Component.prototype =
+    /**
+     * @lends jBlocks.Component
+     */
+    {
+        /**
+         * Subscribe component to the event
+         * @param  {String}   event
+         * @param  {Function} callback
+         * @return {jBlocks.Component}
+         */
+        on: function(event, callback) {
+            this.__events[event] = this.__events[event] || [];
 
-        if (this.__events[event].indexOf(callback) === -1) {
-            this.__events[event].push(callback);
-        }
-        return this;
-    };
-
-    Component.prototype.off = function(event, callback) {
-        var callbacks = this.__events[event];
-        if (!callbacks) {
+            if (this.__events[event].indexOf(callback) === -1) {
+                this.__events[event].push(callback);
+            }
             return this;
-        }
-        if (callbacks.indexOf(callback) > -1) {
-            callbacks.splice(callbacks.indexOf(callback) - 1, 1);
-        } else if (!callback) {
-            this.__events[event] = [];
-        }
-        return this;
-    };
+        },
 
-    Component.prototype.emit = function(event, data) {
-        var callbacks = this.__events[event];
-
-        if (!callbacks) {
+        /**
+         * Unsubscribe component of the event
+         * @param  {String}   event
+         * @param  {Function} callback
+         * @return {jBlocks.Component}
+         */
+        off: function(event, callback) {
+            var callbacks = this.__events[event];
+            if (!callbacks) {
+                return this;
+            }
+            if (callbacks.indexOf(callback) > -1) {
+                callbacks.splice(callbacks.indexOf(callback) - 1, 1);
+            } else if (!callback) {
+                this.__events[event] = [];
+            }
             return this;
-        }
-        var l = i = callbacks.length;
+        },
 
-        while (l--) {
-            try {
-                callbacks[l - i - 1].call(this, data || {});
-            } catch(e) {
-                throw new Error(e.message + '. Check out ' + event + ' handler.');
+        /**
+         * Emit specified event so subscribers can respond
+         * @param  {String} event
+         * @param  {*} data
+         * @return {jBlocks.Component}
+         */
+        emit: function(event, data) {
+            var callbacks = this.__events[event];
+
+            if (!callbacks) {
+                return this;
             }
-        }
-        return this;
-    };
+            var l = i = callbacks.length;
 
-    Component.prototype.destroy = function() {
-        instances[this.name][this.__id] = null;
-        this.__unbindDomEvents();
-        this.__events = null;
-        this.ondestroy();
-        return null;
-    };
-
-    Component.prototype.__bindMethods = function() {
-        var methods = this.decl.methods || {};
-
-        methods.oninit = methods.oninit || noop;
-        methods.ondestroy = methods.ondestroy || noop;
-
-        for (var name in methods) {
-            if (methods.hasOwnProperty(name)) {
-                this[name] = methods[name];
+            while (l--) {
+                try {
+                    callbacks[l - i - 1].call(this, data || {});
+                } catch(e) {
+                    throw new Error(e.message + '. Check out ' + event + ' handler.');
+                }
             }
-        }
-    };
+            return this;
+        },
 
-    Component.prototype.__bindDomEvents = function() {
-        this.__forEachEvent(function(event) {
-            this.node.addEventListener(event, this.__handlerDomEvents);
-        });
-    };
+        /**
+         * Destroy the component
+         * @return {jBlocks.Component}
+         */
+        destroy: function() {
+            instances[this.name][this.__id] = null;
+            this.__unbindDomEvents();
+            this.__events = null;
+            this.ondestroy();
+            return null;
+        },
 
-    Component.prototype.__unbindDomEvents = function() {
-        this.__forEachEvent(function(event) {
-            this.node.removeEventListener(event, this.__handlerDomEvents);
-        });
-    };
+        /**
+         * Bind methods from decl to the instance
+         * @private
+         * @return {jBlocks.Component}
+         */
+        __bindMethods: function() {
+            var methods = this.__decl.methods || {};
 
-    Component.prototype.__forEachEvent = function(callback) {
-        var events = this.decl.events || {};
+            methods.oninit = methods.oninit || noop;
+            methods.ondestroy = methods.ondestroy || noop;
 
-        for (var name in events) {
-            if (events.hasOwnProperty(name)) {
-                var parts = name.split(' ', 2);
-                var event = parts[0];
-                var selector = parts[1];
-                var callbackName = events[name];
-
-                callback.call(this, event, selector, callbackName);
+            for (var name in methods) {
+                if (methods.hasOwnProperty(name)) {
+                    this[name] = methods[name];
+                }
             }
-        }
-    };
+            return this;
+        },
 
-    Component.prototype.__handlerDomEvents = function(e) {
-        var type = e.type;
-        var target = e.target;
-        var events = this.decl.events;
+        /**
+         * Bind DOM Events from decl
+         * @private
+         * @return {jBlocks.Component}
+         */
+        __bindDomEvents: function() {
+            return this.__forEachEvent(function(event) {
+                this.node.addEventListener(event, this.__handlerDomEvents);
+            });
+        },
+        /**
+         * Unbind DOM Events from decl
+         * @private
+         * @return {jBlocks.Component}
+         */
+        __unbindDomEvents: function() {
+            return this.__forEachEvent(function(event) {
+                this.node.removeEventListener(event, this.__handlerDomEvents);
+            });
+        },
 
-        this.__forEachEvent(function(event, selector, callbackName) {
-            if (selector) {
-                var node = this.node.querySelector(selector);
-                if (this.__contains(node, target)) {
+        /**
+         * Iterate for each event from decl
+         * @private
+         * @param  {Function} callback
+         * @return {jBlocks.Component}
+         */
+        __forEachEvent: function(callback) {
+            var events = this.__decl.events || {};
+
+            for (var name in events) {
+                if (events.hasOwnProperty(name)) {
+                    var parts = name.split(' ', 2);
+                    var event = parts[0];
+                    var selector = parts[1];
+                    var callbackName = events[name];
+
+                    callback.call(this, event, selector, callbackName);
+                }
+            }
+            return this;
+        },
+
+        /**
+         * Handler for each distinc event from decl
+         * @private
+         * @param  {Event} e
+         * @return {jBlocks.Component}
+         */
+        __handlerDomEvents: function(e) {
+            var type = e.type;
+            var target = e.target;
+            var events = this.__decl.events;
+
+            this.__forEachEvent(function(event, selector, callbackName) {
+                if (selector) {
+                    var node = this.node.querySelector(selector);
+                    if (this.__contains(node, target)) {
+                        this.__tryCall(callbackName);
+                    }
+                } else {
                     this.__tryCall(callbackName);
                 }
-            } else {
-                this.__tryCall(callbackName);
-            }
-        });
-    };
+            });
+        },
 
-    Component.prototype.__tryCall = function(method) {
-        try {
-            return this[method]();
-        } catch (e) {
-            throw new Error(e.message + '. Check out ' + method);
+        /**
+         * Safely try to call method of component
+         * @private
+         * @param  {String} method
+         * @return {*}
+         */
+        __tryCall: function(method) {
+            try {
+                return this[method]();
+            } catch (e) {
+                throw new Error(e.message + '. Check out ' + method);
+            }
+        },
+
+        /**
+         * Check is one element down from another in DOM
+         * @private
+         * @param  {HTMLElement} root
+         * @param  {HTMLElement} child
+         * @return {Boolean}
+         */
+        __contains: function(root, child) {
+            var root = root.nodeType === 9
+                ? root.documentElement
+                : root;
+            var parent = child && child.parentNode;
+
+            return (
+                root === child ||
+                !!(child && child.nodeType === 1 && parent.contains(child))
+            );
         }
     };
 
-    Component.prototype.__contains = function(root, child) {
-        var root = root.nodeType === 9
-            ? root.documentElement
-            : root;
-        var parent = child && child.parentNode;
-
-        return (
-            root === child ||
-            !!(child && child.nodeType === 1 && parent.contains(child))
-        );
-    };
-
-    return {
-        /**
-         * Destroys instance binded to the node
-         * @param  {HTMLElement} node
-         * @return {jBlocks}
-         */
-        destroy: function(node) {
-            this.get(node).destroy();
-            return this;
-        },
-
-        /**
-         * Defines a new component
-         * @param  {String} name
-         * @param  {Object} declaration
-         * @return {jBlocks}
-         */
-        define: function(name, declaration) {
-            if (declarations[name]) {
-                throw new Error(name + ' has already been declared');
-            }
-            declarations[name] = declaration || {};
-            return this;
-        },
-
-        /**
-         * Removes declaration from cache
-         * @param  {String} name name of component
-         * @return {jBlocks}
-         */
-        forget: function(name) {
-            declarations[name] = null;
-            return this;
-        },
-
-        /**
-         * Creates and returns a new instance of component
-         * @param  {HTMLElement}  node
-         * @return {Component} a new instance
-         */
-        get: function(node) {
-            if (!node) {
-                throw new Error('invalid node');
-            }
-            var name = node.getAttribute('data-component');
-            if (!name) {
-                throw new Error('data-component attribute is missing')
-            }
-            var instanceId = node.getAttribute('data-instance');
-            if (!instanceId) {
-                try {
-                    var props = JSON.parse(node.getAttribute('data-props'));
-                } catch (e) {
-                    throw e;
-                }
-                var instance = new Component(node, name, props);
-                var instanceId = instance.__id;
-
-                if (!instances[name]) {
-                    instances[name] = {};
-                }
-                instances[name][instanceId] = instance;
-            }
-            return instances[name][instanceId];
-        }
-    };
+    return jBlocks;
 }());
